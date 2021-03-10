d193c98b83d5433030f39f18dde66876b63a540b
sunny <sunny@jbcole.co.uk>
2021-03-10 11:24:02 +0000

    Created test suite for async function

--- asyncScript.test.js
+++ asyncScript.test.js
@@ -1,3 +1,13 @@
 const fetch = require('node-fetch');
-const swapi = require('./asyncScript');
+const getPeople = require('./asyncScript');
 
+describe('swapi tests', () => {
+  // With async tests, we specify the number of expected assertions
+  // If using the fetch().then() syntax, we have to make sure to return the promise for the tests to work
+  it('calls swapi to get people', async () => {
+    expect.assertions(2);
+    const data = await getPeople(fetch);
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
